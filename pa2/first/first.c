5f81bf8ddb2742d395ba0739b363f5413e0fb69c
AlfonsoB15 <abuono1599@yahoo.com>
2018-10-20 18:16:23 -0400

    Did backtracking for Sudoku solver. Unsure if I can use that for second part as well.

--- pa2/first/first.c
+++ pa2/first/first.c
@@ -13,17 +13,110 @@ void printMatrix(double**,int,int);
 
 double multiplyRowCol(double*,double**,int,int);
 
+void printVector(double*,int);
+
+double* computeWeights(double**,double*,int,int);
+
 int main(int argc,char** argv){
-    double** matrix = makeIdentity(3,3,9);
-    printMatrix(matrix,3,3);
-    double** matrix2 = makeIdentity(3,3,2);
-    printMatrix(matrix2,3,3);
-    printf("\n");
-    printMatrix(multiplyMatrix(matrix,matrix2,3,3,3,3),3,3);
-    printf("\n");
+    
+    // check the argument values and check if there is the correct amount
+    if(argc < 3 ){
+        printf("error\n");
+        return 0;
+    }
+
+    FILE* fp;
+    fp = fopen(argv[1],"r");
+    // open the file and check if it is the correct file
+    if(fp == NULL){
+        printf("error\n");
+        return 0;
+    }
+
+    // read the file and store the appropriate values
+    // the first line is the number of attributes
+    int numCols;
+    fscanf(fp,"%d",&numCols);
+    numCols++; // have to account for the col of 1's
+    int numRows;
+    fscanf(fp,"%d",&numRows);
+    int counter = 0;    
+    int counter2 = 0;
+    // make the price matrix
+    double* y = (double*)malloc(numRows*sizeof(double));
+    // make the matrix that stores the data
+    double** x = (double**)malloc(numRows*sizeof(double*)); 
+    // malloc each row
+    for(counter = 0;counter<numRows;counter++){
+        x[counter] = (double*)malloc(numCols*sizeof(double));
+    }
+    // increase the number of cols by 1 because of the col of 1s
+    // the last col (the prices) is not included in this count
+    for(counter=0;counter<numRows;counter++){
+        for(counter2=0;counter2<(numCols+1);counter2++){
+            // if col is first col, put a one in it
+            if(counter2 == 0){
+                x[counter][counter2] = 1;
+            }else if(counter2 < numCols){
+                fscanf(fp,"%lf,",&x[counter][counter2]);
+            }else if(counter2 == numCols){
+                // we are at the last col so put these values
+                // into the price matrix
+                fscanf(fp,"%lf",&y[counter]);
+            }
+        }
+    }
+
+    // i have the vector y (the prices) as well as
+    // the matrix x which contains all of the values of the
+    // attributes. we will now use the equation 
+    // W = (X^T * X)^-1 * (X^T) *Y
+    // where W is the matrix that contains the values of the weights
+
+    double* w = (double*)malloc(numRows*sizeof(double));
+    
+    w = computeWeights(x,y,numRows,numCols);
     
 
+    printMatrix(x,numRows,numCols);
+    printf("\n");
+    printVector(y,numRows);
+    printf("\n");
+    printVector(w,numRows);
+
     return 0;
+}
+
+double* multiplyVector(double** matrix,double* vector,int rowCount1,int rowCount2,int colCount){
+    double* result = (double*)malloc(rowCount1*sizeof(double));
+    int counter = 0;
+    int counter2 = 0;    
+    int total = 0;
+    
+    for(counter=0;counter<rowCount1;counter++){
+        for(counter2=0;counter2<colCount;counter2++){
+            total+= matrix[counter][counter2]*vector[counter2];        
+        }
+        result[counter] = total;
+        total = 0;
+    }
+
+
+    return result;
+}
+
+double* computeWeights(double** x,double* y, int rowCount,int colCount){
+    // W = (X^T * X)^-1 * (X^T) *Y
+    // where W is the matrix that contains the values of the weights
+
+    double* result = (double*)malloc(rowCount*sizeof(double));
+    // result is going to store the values of the weights
+    
+    
+    result = multiplyVector( multiplyMatrix(computeInverse(multiplyMatrix(computeTranspose(x,rowCount,colCount),x,rowCount,rowCount,colCount,colCount),rowCount,colCount),computeTranspose(x,rowCount,colCount),rowCount,rowCount,colCount,colCount),y,rowCount,rowCount,colCount );
+
+    return result;
+
 }
 
 double** computeTranspose(double** matrix,int rowCount,int numCount){
@@ -79,6 +172,7 @@ double** computeInverse(double** matrix,int rowCount,int colCount){
     double** iMatrix = (double**)malloc(rowCount*sizeof(double*));
     double** augMatrix = (double**)malloc(rowCount*sizeof(double));
     int x,y;
+    int newColCount = rowCount+colCount;
 
     // intialize the rows and make each the length of the original column count + amount of rows
     for(x=0;x<rowCount;x++)
@@ -101,10 +195,29 @@ double** computeInverse(double** matrix,int rowCount,int colCount){
     }
     
     // do the actual row operations
-    
-    iMatrix = matrix;
+    for(x=0;x<rowCount;x++){
+        // loop through all of the rows and make sure that they are put into RREF
+        for(y=0;y<newColCount;y++){
+            // loop through all of the cols and do the row operation
+            if(y==0){
+                // first time looping through this row. Change the pivot to 1
+                
+            }
+        }
+    } 
+    iMatrix = matrix; // this isjust a placeholder while
+                      // im waiting to code the actual row operations
 
     return iMatrix;
+}
+
+void printVector(double* vector,int row){
+    int x;
+    for(x=0;x<row;x++){
+        printf("%lf\n",vector[x]);
+    }
+
+    return;
 }
 
 void printMatrix(double** matrix,int row,int col){
