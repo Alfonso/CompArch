7dfe9e2d8bad719f738564004a29bac236cf4ef4
AlfonsoB15 <abuono1599@yahoo.com>
2018-11-28 22:02:31 -0500

    Did argument stuff

--- pa4/first.c
+++ pa4/first.c
@@ -1,6 +1,68 @@
 #include"first.h"
 
 int main(int argc,char** argv){
+    int cacheSize;
+    int n;
+    char* policy;
+    int blockSize;
+    FILE* trace;
+// check arguments and stuff
+    if(argc!=6){
+        printf("error");
+        printf("Input correct # of args\n");
+        return -1;
+    }
+
+    cacheSize = atoi(argv[1]);
+    if(powerOfTwo(cacheSize) == 0 ){
+        printf("error");
+        printf("Cache Size: %d is not a power of two\n",cacheSize);
+        return -1;
+    }
+
+    n = setAssoc(argv[2]);
+    if(n==-1){
+        // direct
+    }else if(n==-2){
+        // assoc
+    }else if(n==-3){
+        printf("error");
+        printf("Associativity is incorrect\n");
+        return -1;
+    }else{
+        // assoc:n
+        // check if n is a power of 2
+        if(powerOfTwo(n) == 0){
+            printf("error");
+            printf("N is not a power of 2\n");
+            return -1;
+        }
+    }
+
+    policy = argv[3];
+    if(strcmp(policy,"fifo") != 0){
+        printf("error");
+        printf("Cache policy is not valid\n");
+        return -1;
+    }
+
+    blockSize = atoi(argv[4]);
+    if(powerOfTwo(blockSize) == 0 ){
+        printf("error");
+        printf("Block Size: %d is not a power of two\n",blockSize);
+        return -1;
+    }
+    
+    trace = fopen(argv[5],"r");
+    if(trace == NULL){
+        printf("error");
+        printf("Trace file is not valid\n");
+        return -1;
+    }
+
+// argument setup is done
+// actually do cache stuff now
+
 
     return 1;
 }
--- pa4/first.h
+++ pa4/first.h
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<math.h>
 #include<string.h>
+#include<ctype.h>
 
 // returns whether or not the given num is a power of two
 // returns 1 if it is a power of two
@@ -12,3 +13,34 @@ int powerOfTwo(int num){
         return 0;
     return ((num & (num-1))==0);
 }
+
+// checks to see the associativity
+// returns -3 if error
+// returns -1 if it is direct
+// returns -2 if it is assoc
+// returns n if it is assoc:n
+int setAssoc(char* assoc){
+
+    char* buff = (char*)malloc(sizeof(char)*(1+strlen(assoc))); 
+
+    strcpy(buff,assoc);
+    
+    char* token = strtok(buff,":");
+    token = strtok(NULL,":");
+    
+    // if token is null, either direct, assoc, or error
+    if( token == NULL ){
+        if(strcmp(assoc,"direct")==0)
+            return -1;
+        else if(strcmp(assoc,"assoc")==0)
+                return -2;
+            else return -3;
+    }
+
+    // not null, so return n
+    if(isdigit(token)){
+        return atoi(token);
+    }
+    return -3;
+}
+
